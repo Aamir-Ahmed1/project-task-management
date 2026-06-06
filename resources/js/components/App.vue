552f849c7092542a835ecc419ae4a06b950edbe7
AamirAhmed1 <aamir.ahmed@techstreet.in>
2026-06-06 15:57:13 +0530

    Fix flash message lost on route change by moving flash provider to root App.vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,3 +1,24 @@
 <template>
-    <router-view />
+    <div>
+        <div v-if="flashMessage" class="position-fixed top-0 start-50 translate-middle-x mt-2" style="z-index: 9999;">
+            <div :class="'alert alert-' + flashType + ' alert-dismissible fade show shadow'" role="alert">
+                {{ flashMessage }}
+                <button type="button" class="btn-close" @click="flashMessage = null"></button>
+            </div>
+        </div>
+        <router-view />
+    </div>
 </template>
+
+<script setup>
+import { ref, provide } from 'vue'
+
+const flashMessage = ref(null)
+const flashType = ref('success')
+
+provide('flash', (message, type = 'success') => {
+    flashMessage.value = message
+    flashType.value = type
+    setTimeout(() => flashMessage.value = null, 5000)
+})
+</script>
--- resources/js/components/AppLayout.vue
+++ resources/js/components/AppLayout.vue
@@ -4,10 +4,6 @@
         <div class="flex-grow-1 d-flex flex-column">
             <Navbar />
             <main class="flex-grow-1 p-4 bg-light">
-                <div v-if="flashMessage" :class="'alert alert-' + flashType + ' alert-dismissible fade show'" role="alert">
-                    {{ flashMessage }}
-                    <button type="button" class="btn-close" @click="flashMessage = null"></button>
-                </div>
                 <slot />
             </main>
         </div>
@@ -15,16 +11,6 @@
 </template>
 
 <script setup>
-import { ref, provide } from 'vue'
 import Sidebar from './Sidebar.vue'
 import Navbar from './Navbar.vue'
-
-const flashMessage = ref(null)
-const flashType = ref('success')
-
-provide('flash', (message, type = 'success') => {
-    flashMessage.value = message
-    flashType.value = type
-    setTimeout(() => flashMessage.value = null, 5000)
-})
 </script>
